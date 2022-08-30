188364ef7d7cb37b78aca80816d8cb2e65d7e8be
Choiyu330 <jenny4063@naver.com>
2022-08-30 13:12:17 +0900

    String 타입의 변수선언과 할당

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,15 @@ public class Main {
         // 오버플로우 발생 -> -128 출력
         System.out.println(byteValue);
 
+        // String 타입의 변수선언과 할당
+        // 1. 문자열 리터럴을 String 타입의 변수 name에 할당하는 방법
+        String name1 = "Choi Yujin";
+
+        // 2. String 클래스의 인스턴스를 생성하는 방법
+        String name2 = new String("Choi Yujin");
+
+        System.out.println("방법 1 : " + name1);
+        System.out.println("방법 2 : " + name2);
     }
 }
 
