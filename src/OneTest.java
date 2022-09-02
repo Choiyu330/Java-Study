7a36864a02e3427358af9f626e72081d92283f6e
Choiyu330 <jenny4063@naver.com>
2022-09-02 23:54:26 +0900

    Static과 Instance

---
+++ src/OneTest.java
@@ -0,0 +1,23 @@
+public class OneTest {
+    public static void main(String[] args) {
+        One staticField1 = new One(); // 객체 생성
+        One staticField2 = new One();
+
+        staticField1.num1 = 100;
+        staticField2.num1 = 1000;
+
+        System.out.println(staticField1.num1);
+        System.out.println(staticField2.num1);
+
+        staticField1.num2 = 150;
+        staticField2.num2 = 1500;
+        System.out.println(staticField1.num2);
+        System.out.println(staticField2.num2);
+
+    }
+}
+
+class One {
+    int num1 = 10;
+    static int num2 = 15;
+}
