8a16855b2a13681ec5e77bd9e16ea9c94d8f37bd
Choiyu330 <jenny4063@naver.com>
2022-09-13 23:48:33 +0900

    두 개의 문자열을 입력받아 서로같은지 여부 리턴하기

---
+++ src/compareOnlyAlphabet.java
@@ -0,0 +1,8 @@
+public class compareOnlyAlphabet {
+    public boolean compareOnlyAlphabet(String str1, String str2) {
+        if (str1.equalsIgnoreCase(str2))
+            return true;
+
+        return false;
+    }
+}
