f258e93bd56a818e178a45787c496d32183b1b67
Choiyu330 <jenny4063@naver.com>
2023-04-17 10:41:00 +0000

    [Assignment] 동적 할당법

--- src/DailyCoding/Solution.java
+++ src/DailyCoding/Solution.java
@@ -1,28 +1,18 @@
 package DailyCoding;
 
-import java.util.Arrays;
-
 class Solution {
     public static void main(String[] args) {
-        System.out.println(solution(1,13,1));
+        System.out.println(solution(0, new int[]{1,2,3}));
     }
-    public static String solution(String my_string) {
-
-        String answer = "";
-        my_string = my_string.toLowerCase();
-        String[] str = new String[my_string.length()];
-
-        for(int i=0; i<my_string.length(); i++) {
-            str[i] = String.valueOf(my_string.charAt(i));
+    public static int solution(int sum, int[] coins) {
+        int[] answer = new int[sum + 1];
+        // sum 이 0일 때
+        answer[0] = 1;
+        for (int coin : coins) {
+            for (int i=coin; i<=sum; i++) {
+                answer[i] += answer[i - coin];
+            }
         }
-
-        Arrays.sort(str);
-
-        answer = Arrays.stream(str).collect(Collectors.joining());
-        System.out.println(str);
-        System.out.println(Arrays.toString(str));
-        System.out.println("" + Arrays.toString(str));
-        return answer;
+        return answer[sum];
     }
 }
-
